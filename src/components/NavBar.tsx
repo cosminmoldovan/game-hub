52064f25f56af1aa62a74d5dbfe03da610ea9d3a
Cosmin Moldovan <cosminmol@proton.me>
2023-04-08 19:28:26 +0300

    change logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,16 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
-import logo from "../assets/logo.webp";
+import { Flex, HStack, Image, Text, useColorMode } from "@chakra-ui/react";
+import logo from "../assets/logo.svg";
+import logoDark from "../assets/logo-dark.svg";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
+  const {colorMode } = useColorMode();
   return (
-    <HStack justifyContent={"space-between"} px={'20px'} py={'10px'}>
-      <Image src={logo} boxSize={"60px"} />
+    <HStack justifyContent={"space-between"} px={"30px"} py={"20px"}>
+      <Flex gap={2} alignItems={'center'}>
+        <Image src={colorMode === "dark" ? logo : logoDark} boxSize={"40px"} />
+        <Text pt={3} fontWeight={'bold'}>GameHub</Text>
+      </Flex>
       <ColorModeSwitch />
     </HStack>
   );
