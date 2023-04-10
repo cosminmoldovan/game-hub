6db89da52d3b91c21e8094bdec1c511d6db7cd27
Cosmin Moldovan <cosminmol@proton.me>
2023-04-10 12:09:52 +0300

    refactor: remove duplicate styles

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
-export interface Platform{
+export interface Platform {
   id: number;
   name: string;
   slug: string;
@@ -11,7 +11,7 @@ export interface Game {
   id: number;
   name: string;
   background_image: string;
-  parent_platforms:  {platform: Platform}[]
+  parent_platforms: { platform: Platform }[];
 }
 interface FetchGameResponse {
   count: number;
@@ -21,18 +21,24 @@ interface FetchGameResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
   useEffect(() => {
     const controller = new AbortController();
+    setLoading(true);
     apiClient
-      .get<FetchGameResponse>("/games", {signal: controller.signal})
-      .then((res) => setGames(res.data.results))
+      .get<FetchGameResponse>("/games", { signal: controller.signal })
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
-        if(err instanceof CanceledError) return;
+        if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
 
-      return ()=> controller.abort();
+    return () => controller.abort();
   }, []);
-  return { games, error, setGames };
+  return { games, error, isLoading };
 };
 export default useGames;
