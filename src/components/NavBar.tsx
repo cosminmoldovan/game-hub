091d49cf2fdab425e0c228db99152e951f2ddbca
Cosmin Moldovan <cosminmol@proton.me>
2023-04-13 10:09:12 +0300

    build search input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,15 +2,14 @@ import { Flex, HStack, Image, Text, useColorMode } from "@chakra-ui/react";
 import logo from "../assets/logo.svg";
 import logoDark from "../assets/logo-dark.svg";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
-  const {colorMode } = useColorMode();
+  const { colorMode } = useColorMode();
   return (
-    <HStack justifyContent={"space-between"} py={"20px"}>
-      <Flex gap={2} alignItems={'center'}>
-        <Image src={colorMode === "dark" ? logo : logoDark} boxSize={"40px"} />
-        <Text pt={3} fontWeight={'bold'}>GameHub</Text>
-      </Flex>
+    <HStack py="25px" mb={30}>
+      <Image src={colorMode === "dark" ? logo : logoDark} boxSize={"40px"}/>
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
