73fd5e70d1689f54b628e179de769a41dd085917
Cosmin Moldovan <cosminmol@proton.me>
2023-04-13 11:43:50 +0300

    fix genre query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,5 @@
 import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenres";
 export interface Platform {
   id: number;
   name: string;
@@ -17,7 +16,7 @@ const useGames = (gameQuery: GameQuery) =>
     "/games",
     {
       params: {
-        genres: gameQuery.platform?.id,
+        genres: gameQuery.genre?.id,
         platforms: gameQuery.platform?.id,
         ordering: gameQuery.sortOrder,
         search: gameQuery.searchText,
